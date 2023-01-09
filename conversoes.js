5d1808c38faf4e72a3bf161ab49f30bb2753603e
Rodrigo Cattari Lima De Mello <rodrigo.cattari@culturainglesa.com.br>
2023-01-09 16:01:27 -0300

    codigos var e conversoes

---
+++ conversoes.js
@@ -0,0 +1,15 @@
+// tipo de dado
+// booleanos
+
+//conversão implícita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero == numeroString)
+
+//Numer() converte em numero
+//String() converte em String
+
+console.log(numero + Number(numeroString))
+
+//conversão explícita
---
+++ truthy-falsy.js
@@ -0,0 +1,9 @@
+//boolean
+const usuarioLogado = true;
+const contapaga = false;
+
+// truthy ou falsy
+ 
+console.log(0 == false);
+console.log(""== false);
+console.log(1 == true);
