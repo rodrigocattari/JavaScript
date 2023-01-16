4e682b06271f433a195418207db6cdb1509dd2d8
Rodrigo Cattari Lima De Mello <rodrigo.cattari@culturainglesa.com.br>
2023-01-16 17:00:54 -0300

    Estudos 16/01/2023

---
+++ atualizando-lista.js
@@ -0,0 +1,7 @@
+const nomes = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
+
+nomes.splice(1, 2, "Rodrigo");  //Ultimo parametro acrecenta o valor no lugar do que foi retirado
+
+// nomes.push("Rodrigo");
+
+console.log(nomes);
